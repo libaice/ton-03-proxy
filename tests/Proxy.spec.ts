badee7ca28322e86e3e3c55f16173b350a4e6e23
libaice <libaice147@gmail.com>
2024-08-29 07:45:14 +0400

    add proxy contract test

--- tests/Proxy.spec.ts
+++ tests/Proxy.spec.ts
@@ -1,8 +1,8 @@
-import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
-import { Cell, toNano } from '@ton/core';
-import { Proxy } from '../wrappers/Proxy';
+import {Blockchain, SandboxContract, TreasuryContract} from '@ton/sandbox';
+import {beginCell, Cell, toNano} from '@ton/core';
+import {Proxy} from '../wrappers/Proxy';
 import '@ton/test-utils';
-import { compile } from '@ton/blueprint';
+import {compile} from '@ton/blueprint';
 
 describe('Proxy', () => {
     let code: Cell;
@@ -17,23 +17,50 @@ describe('Proxy', () => {
 
     beforeEach(async () => {
         blockchain = await Blockchain.create();
-
-        proxy = blockchain.openContract(Proxy.createFromConfig({}, code));
-
         deployer = await blockchain.treasury('deployer');
+        proxy = blockchain.openContract(Proxy.createFromConfig({
+            owner: deployer.address,
+        }, code));
 
-        const deployResult = await proxy.sendDeploy(deployer.getSender(), toNano('0.05'));
+        const deployResult = await proxy.sendDeploy(deployer.getSender(), toNano('0.01'));
 
         expect(deployResult.transactions).toHaveTransaction({
             from: deployer.address,
             to: proxy.address,
             deploy: true,
-            success: true,
+            // success: true,
         });
     });
 
-    it('should deploy', async () => {
-        // the check is done inside beforeEach
-        // blockchain and proxy are ready to use
+    it('should not forward from owner', async () => {
+        const result = await deployer.send({
+            to: proxy.address,
+            value: toNano('1'),
+        })
+        expect(result.transactions).not.toHaveTransaction({
+            from: proxy.address,
+            to: deployer.address,
+        });
+    });
+
+    it('should forward from another wallet', async () => {
+        let user = await blockchain.treasury('user');
+        const result = await user.send({
+            to: proxy.address,
+            value: toNano('1'),
+            body: beginCell().storeStringTail('Hello, world!').endCell(),
+        });
+        expect(result.transactions).toHaveTransaction({
+            from: proxy.address,
+            to: deployer.address,
+            body: beginCell()
+                .storeAddress(user.address)
+                .storeRef(
+                    beginCell().storeStringTail('Hello, world!').endCell()
+                )
+                .endCell(),
+            value: (x) => (x ? toNano('0.99') <= x && x <= toNano('1') : false),
+        });
+
     });
 });
--- wrappers/Proxy.ts
+++ wrappers/Proxy.ts
@@ -1,13 +1,17 @@
-import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from '@ton/core';
+import {Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode} from '@ton/core';
 
-export type ProxyConfig = {};
+export type ProxyConfig = {
+    owner: Address;
+
+};
 
 export function proxyConfigToCell(config: ProxyConfig): Cell {
-    return beginCell().endCell();
+    return beginCell().storeAddress(config.owner).endCell();
 }
 
 export class Proxy implements Contract {
-    constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {}
+    constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {
+    }
 
     static createFromAddress(address: Address) {
         return new Proxy(address);
@@ -15,7 +19,7 @@ export class Proxy implements Contract {
 
     static createFromConfig(config: ProxyConfig, code: Cell, workchain = 0) {
         const data = proxyConfigToCell(config);
-        const init = { code, data };
+        const init = {code, data};
         return new Proxy(contractAddress(workchain, init), init);
     }
 
